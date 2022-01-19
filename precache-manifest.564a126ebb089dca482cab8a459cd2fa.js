f90384403294b7ff9b6e926c4ab585dcede1b0b6
ishaan191220 <loombaishaan@gmail.com>
2022-01-19 15:22:37 +0530

    Updates

---
+++ precache-manifest.564a126ebb089dca482cab8a459cd2fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "444599871ac4d5326eb5",
+    "url": "/market3/static/css/main.3c2a4113.chunk.css"
+  },
+  {
+    "revision": "444599871ac4d5326eb5",
+    "url": "/market3/static/js/main.44459987.chunk.js"
+  },
+  {
+    "revision": "88ca46333341a3a3c4d3",
+    "url": "/market3/static/css/1.67859e0e.chunk.css"
+  },
+  {
+    "revision": "88ca46333341a3a3c4d3",
+    "url": "/market3/static/js/1.88ca4633.chunk.js"
+  },
+  {
+    "revision": "05e03e2acda8294ec07c",
+    "url": "/market3/static/js/runtime~main.05e03e2a.js"
+  },
+  {
+    "revision": "aa91157eed56ad3a0f8ee0b314e73afe",
+    "url": "/market3/index.html"
+  }
+];
